8f783018bea64dc5141f439cff29c83008552f83
Brandon Block <brandon.block@gmail.com>
2018-01-31 16:09:15 -0600

    ticker and mutex

--- channeler.go
+++ channeler.go
@@ -7,16 +7,19 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 )
 
 type Channeler struct {
 	requests           chan http.Request
 	transactions       chan Transaction
+	blocks             chan Block
 	lastHash           string
-	lastWrite          time.Time
 	activeBlock        Block
 	activeTransactions []Transaction
+	ticker             *time.Ticker
+	mux                sync.Mutex
 }
 
 type Transaction struct {
@@ -62,26 +65,37 @@ func (ch *Channeler) transact() {
 }
 
 func (ch *Channeler) record() {
+	go func() {
+		for {
+			select {
+			case <-ch.ticker.C:
+				fmt.Println("tick")
+				ch.activeBlock.transactions = ch.activeTransactions
+				if len(ch.activeBlock.transactions) > 0 {
+					hasher := md5.New()
+					ch.mux.Lock()
+					hasher.Write([]byte(fmt.Sprintf("%v%d", ch.activeBlock.transactions, time.Now().Unix())))
+					ch.activeBlock.blockHash = hex.EncodeToString(hasher.Sum(nil))
+					ch.activeBlock.prevBlockHash = ch.lastHash
+					ch.lastHash = ch.activeBlock.blockHash
+					ch.blocks <- ch.activeBlock
+					ch.activeTransactions = []Transaction{}
+					ch.activeBlock = Block{}
+					ch.mux.Unlock()
+				}
+			}
+		}
+	}()
 	for t := range ch.transactions {
 		ch.activeTransactions = append(ch.activeTransactions, t)
-		//TODO: check lastWrite time constantly
-		if ch.lastWrite.Before(time.Now().Add(-30 * time.Second)) {
-			ch.activeBlock.transactions = ch.activeTransactions
-			hasher := md5.New()
-			hasher.Write([]byte(fmt.Sprintf("%v%d", ch.activeBlock.transactions, time.Now().Unix())))
-			ch.activeBlock.blockHash = hex.EncodeToString(hasher.Sum(nil))
-			ch.activeBlock.prevBlockHash = ch.lastHash
-			ch.lastHash = ch.activeBlock.blockHash
-			log.Printf("%+v", ch.activeBlock)
-			ch.activeTransactions = []Transaction{}
-			ch.activeBlock = Block{}
-			ch.lastWrite = time.Now()
-		}
 	}
 }
 
-func (ch *Channeler) write(b Block) {
-	return
+func (ch *Channeler) write() {
+	for block := range ch.blocks {
+		//TODO: write to disk instead of printing
+		log.Printf("%+v", block)
+	}
 }
 
 func extract(r http.Request, lookup string) (value string, err error) {
